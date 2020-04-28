6722457a3f331d19aee288345603c9106c770bdf
jpvail <54407427+jpvail@users.noreply.github.com>
2020-04-28 16:38:49 -0400

    Add files via upload

---
+++ vHashTable/pvaultHT.cpp
@@ -0,0 +1,58 @@
+#include <iostream> 
+#include <fstream> 
+#include <cstring> 
+#include <chrono>
+#include "HT.hh"
+using namespace std; 
+
+string encrypt(string str){
+
+}
+
+string decrypt(string str){
+  
+}
+
+bool checkForDataBase(){
+  
+}
+
+bool checkForAcct(){
+
+}
+
+bool interact_login(string* loginpassword, int* size){
+
+}
+
+void buildHT(HashTable* ht){
+}
+
+void insertNewPassword(HashTable* ht){
+    
+}
+
+void removePassword(HashTable* ht){
+  
+}
+
+void updateDataBase(HashTable* ht, string loginpassword){
+  
+}
+
+
+void getPassword(HashTable* ht){
+   
+}
+
+bool interact_createAcct(){ 
+    
+}
+
+
+int main(int argc, char *argv[]){
+  
+    return 0;
+}
+
+
