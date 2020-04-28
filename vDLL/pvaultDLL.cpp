052e1b13bb7f54ff09dc5261d0d5204c0729276e
jpvail <54407427+jpvail@users.noreply.github.com>
2020-04-28 16:36:26 -0400

    Add files via upload

---
+++ vDLL/pvaultDLL.cpp
@@ -0,0 +1,60 @@
+#include <iostream> 
+#include <fstream> 
+#include <cstring> 
+#include <chrono> 
+#include "DLL.hh"
+using namespace std; 
+
+
+string encrypt(string str){
+
+}
+
+string decrypt(string str){
+
+}
+
+bool checkForDataBase(){
+
+}
+
+bool checkForAcct(){
+
+}
+
+
+bool interact_login(string* loginpassword){
+
+}
+
+void buildDLLStack(DLLStack* dlls){
+
+}
+
+void insertNewPassword(DLLStack* dlls){
+ 
+}
+
+void removePassword(DLLStack* dlls){
+ 
+}
+
+void updateDataBase(DLLStack* dlls, string loginpassword){
+
+}
+
+void getPassword(DLLStack* dlls){
+
+}
+
+
+bool interact_createAcct(){ 
+ 
+}
+
+
+int main(int argc, char *argv[]){
+
+}
+
+
