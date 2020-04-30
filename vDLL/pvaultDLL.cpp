0c15485b432c1cc4f7037dc799872923cb7af71d
Jason Vail <jvail1@binghamton.edu>
2020-04-29 21:21:58 -0400

    first commit

--- vDLL/pvaultDLL.cpp
+++ vDLL/pvaultDLL.cpp
@@ -24,15 +24,68 @@ bool checkForAcct(){
 
 
 bool interact_login(string* loginpassword){
-
+    string inputpassword, stored_password;
+    cout << "Insert login password: "; 
+    cin >> inputpassword;  
+    fstream myfile("database.txt");
+    if(myfile.is_open()){
+        getline(myfile, stored_password);
+        if(decrypt(stored_password).compare(inputpassword) == 0){
+            myfile.close();
+            *loginpassword = stored_password; 
+            return true; 
+        }else{
+            return false; 
+        }
+    }else{
+        cout << "No database file in directory" << endl; 
+        return false;
+    }
 }
 
 void buildDLLStack(DLLStack* dlls){
-
+    ifstream myfile("database.txt"); 
+    string passw_name, passw_real, oldstoredpassword; 
+    myfile >> oldstoredpassword;
+    while (true){
+        myfile >> passw_name;
+        myfile >> passw_real;
+        if(myfile.eof()) break; 
+        //auto start = chrono::high_resolution_clock::now();
+        (*dlls).push(passw_name, decrypt(passw_real));
+        /*auto end = chrono::high_resolution_clock::now();
+        double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count(); 
+        time_taken *= 1e-9; 
+        cout << fixed  
+        << time_taken << setprecision(9); 
+        cout << endl; */
+    }
+    myfile.close(); 
 }
 
 void insertNewPassword(DLLStack* dlls){
- 
+    int insert_another_password = 1;
+    while(insert_another_password){
+        string password_name, password, another; 
+        cout << "Insert name for this password: ";
+        cin >> password_name;   
+        cout << "Insert password (will be encrypted): ";
+        cin >> password;
+        (*dlls).push(password_name, password);
+        int y = 1; 
+        while(y){
+            y = 0;
+            cout << "Would you like to store another (y / n)? ";
+            cin >> another;
+            if(another.compare("y") == 0){
+                insert_another_password = 1;
+            }else if(another.compare("n") == 0){
+                insert_another_password = 0; 
+            }else{
+                y = 1;
+            }
+        }
+    } 
 }
 
 void removePassword(DLLStack* dlls){
@@ -40,16 +93,85 @@ void removePassword(DLLStack* dlls){
 }
 
 void updateDataBase(DLLStack* dlls, string loginpassword){
-
+    ofstream myfile("database.txt", std::ofstream::trunc);
+    myfile << loginpassword << endl;
+    while((*dlls).length() > 0){
+        Pw temp = (*dlls).pop();
+        myfile << temp.password_name << " " << encrypt(temp.password_real) << "\n"; 
+    }
+    myfile.close();
 }
 
 void getPassword(DLLStack* dlls){
-
+    string password_name; 
+    cout << "Name of password to find: ";
+    cin >> password_name; 
+    //auto start = chrono::high_resolution_clock::now();
+    (*dlls).findPassword(password_name); 
+   /* auto end = chrono::high_resolution_clock::now();
+    double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count(); 
+    time_taken *= 1e-9; 
+    cout << fixed  
+    << time_taken << setprecision(9); 
+    cout << endl; */
 }
 
 
 bool interact_createAcct(){ 
- 
+    string pwrd1, pwrd2, quitorcont; 
+    while((pwrd1.size() == 0 && pwrd2.size() == 0) || pwrd1.compare(pwrd2) != 0){
+        cout << "Welcome. Create a password: "; 
+        cin >> pwrd1; 
+        cout << "Confirm new password: "; 
+        cin >> pwrd2; 
+        if(pwrd1.compare(pwrd2) != 0){
+            cout  << "Passwords must match." << endl; 
+        }
+    }
+    fstream myfile("database.txt"); 
+    if(myfile.is_open()){
+        myfile << encrypt(pwrd1) << "\n";
+        int errorcheck = 1; 
+        while(errorcheck){
+            errorcheck = 0; 
+            cout << "Would you like to store new passwords (y / n)? ";
+            cin >> quitorcont;
+            if(quitorcont.compare("y") == 0){
+                int insert_another_password = 1;
+                while(insert_another_password){
+                    string password_name, password, another; 
+                    cout << "Insert name for this password: ";
+                    cin >> password_name;   
+                    cout << "Insert password (will be encrypted): ";
+                    cin >> password;
+                    myfile << password_name << " " << encrypt(password) << '\n'; 
+                    int y = 1; 
+                    while(y){
+                        y = 0;
+                        cout << "Would you like to store another (y / n)? ";
+                        cin >> another;
+                        if(another.compare("y") == 0){
+                            insert_another_password = 1;
+                        }else if(another.compare("n") == 0){
+                            insert_another_password = 0; 
+                        }else{
+                            y = 1;
+                        }
+                    }                                    
+                } 
+                myfile.close(); 
+                return true; 
+            }else if(quitorcont.compare("n") == 0){
+                myfile.close(); 
+                return true; 
+            }else{
+                errorcheck = 1; 
+            }
+        }
+    }else{
+        cout << "No database file in directory" << endl; 
+        return false;
+    }
 }
 
 
