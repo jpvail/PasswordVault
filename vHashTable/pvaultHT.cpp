245388dfad15e9a99d31b31eef89e1948bd736d8
Jason Vail <jvail1@binghamton.edu>
2020-05-02 16:13:37 -0400

    temp

---
+++ vHashTable/pvaultHT.cpp
@@ -0,0 +1,104 @@
+#include <iostream> 
+#include <fstream> 
+#include <cstring> 
+#include <chrono>
+#include "HT.hh"
+using namespace std; 
+
+//same as other version 
+string encrypt(string str){
+    for(int i = 0; i < str.size(); ++i){
+        str[i] -= 5; 
+    }
+    return str; 
+}
+
+//same as other version 
+string decrypt(string str){
+    for(int i = 0; i < str.size(); ++i){
+        str[i] += 5; 
+    }
+    return str; 
+}
+
+//same as other version 
+bool checkForDataBase(){
+    ifstream myfile("database.txt");
+    if(myfile.is_open()){
+        myfile.close(); 
+        return true;
+    }else{
+        cout << "No database in directory" << endl;
+        return false; 
+    }
+}
+
+//same as other version 
+bool checkForAcct(){
+    string password; 
+    ifstream myfile("database.txt"); 
+    if(myfile.is_open()){
+        getline(myfile, password); 
+        myfile.close(); 
+        if(password.size() > 0){
+            return true; 
+        }else{
+            return false;  
+        }
+    }else{
+        cout << "No database file in directory" << endl; 
+        return false;  
+    }
+}
+
+bool interact_login(string* loginpassword, int* size){
+
+}
+
+//O(n)
+void buildHT(HashTable* ht){
+
+}
+
+void insertNewPassword(HashTable* ht){
+
+}
+
+void removePassword(HashTable* ht){
+
+}
+
+void updateDataBase(HashTable* ht, string loginpassword){
+
+}
+
+
+void getPassword(HashTable* ht){
+<<<<<<< HEAD
+
+}
+
+//same as other version 
+bool interact_createAcct(){ 
+
+  
+=======
+
+>>>>>>> 3a5ab0e7321cfad3276c1de5ccf5033d7ce4c8d8
+}
+
+//same as other version 
+bool interact_createAcct(){ 
+
+<<<<<<< HEAD
+int main(int argc, char *argv[]){
+    
+=======
+  
+>>>>>>> 3a5ab0e7321cfad3276c1de5ccf5033d7ce4c8d8
+}
+
+
+int main(int argc, char *argv[]){
+    
+}
