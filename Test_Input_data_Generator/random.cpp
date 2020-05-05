5f0e191b1bc4c9a787f94cd3a90234576d26a9d5
Jason Vail <jvail1@binghamton.edu>
2020-05-04 23:15:57 -0400

    test input data generator

---
+++ Test_Input_data_Generator/random.cpp
@@ -0,0 +1,27 @@
+#include<iostream> 
+#include<memory> 
+using namespace std; 
+
+
+void rword (char *word)
+{
+int len = rand () % 6 + 1;
+word [len] = 0;
+while (len) word [--len] = 'a' + rand () % 26;
+}
+
+int main ()
+{
+	char word[7];
+	char word2[7];
+	int x=0;
+	srand(time(0));
+	while (x<20000) 
+	{
+	  rword(word);
+	  rword(word2);
+	  cout << word << ' ' << word2 << endl; 
+	  //printf ("%s\n", word);
+	  x++;
+	}
+}
